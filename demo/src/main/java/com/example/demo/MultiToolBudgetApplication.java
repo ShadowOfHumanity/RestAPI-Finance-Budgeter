b523e336ca691ae3486df713cb570008e201938b
ShadowOfHumanity <Darianbakerbray@gmail.com>
2025-01-20 14:49:29 +0100

    ADDED TODO ADDED MORE TO DB

--- demo/src/main/java/com/example/demo/MultiToolBudgetApplication.java
+++ demo/src/main/java/com/example/demo/MultiToolBudgetApplication.java
@@ -8,6 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class MultiToolBudgetApplication {
 	// TODO: make it possible on both phone and computer using RController
+	// TODO: ADD EMAIL CONFIRMATION VIA EMAIL SMS
 	// TODO: COMPRESSION HAS TO BE APPLIED CLIENT SIDE
 	public static void main(String[] args) {
 		SpringApplication.run(MultiToolBudgetApplication.class, args);
